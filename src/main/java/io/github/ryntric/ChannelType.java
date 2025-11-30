0e8ef1e24e042078d883c6640ef3b0e70b2cbff1
ryntric <io.github.ryntric@gmail.com>
2025-11-30 21:18:43 +0200

    some fixes

--- src/main/java/io/github/ryntric/ChannelType.java
+++ src/main/java/io/github/ryntric/ChannelType.java
@@ -2,9 +2,8 @@ package io.github.ryntric;
 
 /**
  * Defines the types of channels supported by the system.
- *
- * <li>{@code SPSC} — Single-Producer, Single-Consumer channel.</li>
- * <li>{@code MPSC} — Multi-Producer, Single-Consumer channel.</li>
+ * {@code SPSC} — Single-Producer, Single-Consumer channel.
+ * {@code MPSC} — Multi-Producer, Single-Consumer channel.
  * These types determine the internal concurrency model and
  * behavior of the channel when multiple threads are producing or consuming messages.
  */
